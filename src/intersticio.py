ba7d40b5701584a0c4705b55d8048f4416218957
Gabriela Neme <gabrielaneme@MacBook-Pro-de-Gabriela.local>
2023-04-20 11:45:42 -0300

    Cria intersticio
    O intersticio é o período de tempo em meses necessário para passar de um
    nível a outro. O objetivo dessa classe, então, é informar quanto tempo
    é necessário para "andar" x passos na tabela de cargos, verticalmente.

---
+++ src/intersticio.py
@@ -0,0 +1,25 @@
+from src.cargo import Nivel
+
+
+class Intersticio:
+    @staticmethod
+    def _intersticio(numero_nivel: int) -> int:
+        """Calcula número de meses de intersticio para alcançar determinado nível."""
+        if numero_nivel <= 8:
+            return 9
+        if numero_nivel <= 28:
+            return 12
+        if numero_nivel <= 47:
+            return 15
+        return 24
+
+    @staticmethod
+    def tempo_para_progredir(nivel: Nivel, num_passos: int) -> int:
+        """Calcula o tempo para progredir de um nível a qualquer outro, posterior."""
+
+        tempo = 0
+        numero = nivel.numero
+        for _ in range(num_passos):
+            numero += 1
+            tempo += Intersticio._intersticio(numero)
+        return tempo
---
+++ tests/test_intersticio.py
@@ -0,0 +1,22 @@
+import pytest
+
+from src.cargo import Nivel
+from src.intersticio import Intersticio
+
+
+class TestIntersticio:
+    @pytest.mark.parametrize(
+        "nivel, num_progs, tempo",
+        [
+            (Nivel(1, "0"), 1, 9),
+            (Nivel(1, "A"), 2, 18),
+            (Nivel(7, "B"), 2, 21),
+            (Nivel(15, "C"), 2, 24),
+            (Nivel(27, "D"), 2, 27),
+            (Nivel(30, "E"), 2, 30),
+        ],
+    )
+    def test_calcula_tempo_para_progredir(
+        self, nivel: Nivel, num_progs: int, tempo: int
+    ):
+        assert Intersticio().tempo_para_progredir(nivel, num_progs) == tempo
