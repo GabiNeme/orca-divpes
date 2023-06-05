363d4d8438077ea6ed63fdd10416d9728fa800d7
Gabriela Neme <gabineme@gmail.com>
2023-06-05 11:56:36 -0300

    Refatora classe Carreira
    A classe Carreira atualmente representa a carreira daqueles que foram
    admitidos após 2004.
    
    A fim de incluir a carreira daqueles admitidos antes de 2004 e evitar
    duplicação de código, refatoramos a antiga classe Carreira, e criamos a
    classe filha Carreira2004.

--- src/carreira.py
+++ src/carreira.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from datetime import date
 from typing import Optional
@@ -7,14 +8,6 @@ from dateutil.relativedelta import relativedelta
 from src.cargo import Classe, Nivel
 from src.intersticio import Intersticio
 
-TRANSICAO_PARA_LETRA = [
-    (1, "A"),
-    (7, "B"),
-    (13, "C"),
-    (19, "D"),
-    (25, "E"),
-]
-
 
 @dataclass
 class Progressao:
@@ -22,14 +15,9 @@ class Progressao:
     nivel: Nivel
 
 
-class Carreira2004:
+class Carreira(ABC):
     def __init__(self, classe: Classe) -> None:
-        self.transicao_para_letra = TRANSICAO_PARA_LETRA
-
-        if classe == Classe.E2:
-            self.limite = 37
-        elif classe == Classe.E3:
-            self.limite = 36
+        self.classe = classe
 
     def progride_verticalmente(
         self, ultima_progressao: Progressao, especial: bool
@@ -38,8 +26,9 @@ class Carreira2004:
         ou não."""
 
         self.checa_nivel_valido(ultima_progressao.nivel)
+        limite = self.limite(ultima_progressao.nivel)
 
-        if ultima_progressao.nivel.numero == self.limite:
+        if ultima_progressao.nivel.numero == limite:
             return None
 
         intersticio = Intersticio.tempo_para_progredir(ultima_progressao.nivel, 2)
@@ -50,8 +39,8 @@ class Carreira2004:
         else:
             num_niveis = 2
 
-        if num_niveis + ultima_progressao.nivel.numero > self.limite:
-            num_niveis = self.limite - ultima_progressao.nivel.numero
+        if num_niveis + ultima_progressao.nivel.numero > limite:
+            num_niveis = limite - ultima_progressao.nivel.numero
 
         return Progressao(dt_prox_prog, ultima_progressao.nivel.proximo(num_niveis, 0))
 
@@ -78,18 +67,46 @@ class Carreira2004:
         letra_maxima = self._letra_maxima_para_nivel(nivel_origem.numero)
         return Nivel(nivel_origem.numero, letra_maxima)
 
+    @abstractmethod
+    def _letra_maxima_para_nivel(self, numero_nivel: int) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def checa_nivel_valido(self, nivel: Nivel) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def limite(self, nivel: Nivel = None) -> int:
+        raise NotImplementedError
+
+
+class Carreira2004(Carreira):
+    def limite(self, nivel: Nivel = None) -> int:
+        if self.classe == Classe.E2:
+            return 37
+        if self.classe == Classe.E3:
+            return 36
+
     def _letra_maxima_para_nivel(self, numero_nivel: int) -> str:
         """Retorna a máxima letra que é possível ter para um determinado nível."""
 
-        for i in reversed(range(len(self.transicao_para_letra))):
-            if self.transicao_para_letra[i][0] <= numero_nivel:
-                return self.transicao_para_letra[i][1]
+        transicao_para_letra = [
+            (1, "A"),
+            (7, "B"),
+            (13, "C"),
+            (19, "D"),
+            (25, "E"),
+        ]
+
+        for i in reversed(range(len(transicao_para_letra))):
+            if transicao_para_letra[i][0] <= numero_nivel:
+                return transicao_para_letra[i][1]
 
     def checa_nivel_valido(self, nivel: Nivel):
         """Lança uma exceção se o nível informado for inválido."""
 
         def numero_invalido(numero_nivel: int) -> bool:
-            if numero_nivel > self.limite:
+            if numero_nivel > self.limite(nivel):
                 return True
             return False
 
