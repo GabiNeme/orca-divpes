0cd1d678d657e18bd0cb57d18f4deb9f58a347e2
Gabriela Neme <gabineme@gmail.com>
2025-08-11 15:35:36 -0300

    Cria objeto folha
    Como o nome sugere, a classe Folha representa a folha de pagamento da
    CMBH. Ela é responsável por calcular as despesas da CMBH com aquele
    servidor, e por isso não inclui descontos cuja responsabilidade de
    pagamento é do funcionário.

---
+++ src/folha.py
@@ -0,0 +1,124 @@
+from datetime import date
+
+from dataclasses import dataclass
+from src.anuenio import Anuenio
+from src.funcionario import DadosFolha, TipoPrevidencia
+from src.nivel import Nivel
+from src.tabela_salario import Tabela
+
+LIMITE_PREFEITO = 34604.05
+LIMITE_PROCURADORES = 41845.49
+
+FATOR_PATRONAL = 0.22
+FATOR_PATRONAL_COMPLEMENTAR = 0.085
+TETO_INSS = 8157.41
+
+
+@dataclass
+class Folha:
+    salario: float
+    anuenio: float
+    ats: float
+    total_antes_limite_prefeito: float
+    total: float
+    fufin_patronal: float
+    bhprev_patronal: float
+    bhprev_complementar_patronal: float
+
+
+class CalculaFolha:
+    def __init__(self, funcionario: DadosFolha, tabela: Tabela):
+        self.funcionario = funcionario
+        self.tabela = tabela
+
+    def _calcula_salario(self, nivel: Nivel) -> float:
+        """Calcula o salário base do funcionário."""
+
+        return self.tabela.valor_do_nivel_para_classe(
+            nivel=nivel, classe=self.funcionario.classe
+        )
+
+    def _calcula_anuenio(self, competencia: date) -> float:
+        """Calcula o anuenio do funcionário."""
+
+        anuenio = Anuenio(self.funcionario.data_anuenio)
+        qtde_anuenios = anuenio.obtem_numero_anuenios_para(competencia)
+
+        valor_por_anuenio = 0.01 * self.tabela.valor_do_nivel_para_classe(
+            nivel=Nivel(1, "0"), classe=self.funcionario.classe
+        )
+        return round(valor_por_anuenio * qtde_anuenios, 2)
+
+    def _calcula_ats(self, nivel: Nivel) -> float:
+        """Calcula o ATS do funcionário."""
+
+        salario = self._calcula_salario(nivel)
+        return round(self.funcionario.num_ats * salario * 0.01, 2)
+
+    def _calcula_total_antes_limite_prefeito(
+        self, nivel: Nivel, competencia: date
+    ) -> float:
+        """Calcula o total antes do limite preferencial."""
+        salario = self._calcula_salario(nivel)
+        anuenio = self._calcula_anuenio(competencia)
+        ats = self._calcula_ats(nivel)
+        return round(salario + anuenio + ats, 2)
+
+    def _calcula_total(self, nivel: Nivel, competencia: date) -> float:
+        """Calcula o total do funcionário."""
+        if self.funcionario.procurador:
+            limite = LIMITE_PROCURADORES
+        else:
+            limite = LIMITE_PREFEITO
+
+        total_antes_limite_prefeito = self._calcula_total_antes_limite_prefeito(
+            nivel, competencia
+        )
+        if total_antes_limite_prefeito > limite:
+            return limite
+        return total_antes_limite_prefeito
+
+    def _calcula_fufin_patronal(self, nivel: Nivel, competencia: date) -> float:
+        """Calcula a previdência patronal do funcionário."""
+        if self.funcionario.tipo_previdencia != TipoPrevidencia.Fufin:
+            return 0
+        return round(self._calcula_total(nivel, competencia) * FATOR_PATRONAL, 2)
+
+    def _calcula_bhprev_patronal(self, nivel: Nivel, competencia: date) -> float:
+        """Calcula a previdência patronal do BHPrev do funcionário."""
+        if self.funcionario.tipo_previdencia == TipoPrevidencia.Fufin:
+            return 0
+        elif self.funcionario.tipo_previdencia == TipoPrevidencia.BHPrev:
+            return round(self._calcula_total(nivel, competencia) * FATOR_PATRONAL, 2)
+        elif self.funcionario.tipo_previdencia == TipoPrevidencia.BHPrevComplementar:
+            total = self._calcula_total(nivel, competencia)
+            if total > TETO_INSS:
+                return round(TETO_INSS * FATOR_PATRONAL, 2)
+            return round(total * FATOR_PATRONAL, 2)
+
+    def _calcula_bhprev_complementar_patronal(
+        self, nivel: Nivel, competencia: date
+    ) -> float:
+        """Calcula a previdência patronal complementar do funcionário."""
+        if self.funcionario.tipo_previdencia != TipoPrevidencia.BHPrevComplementar:
+            return 0
+        total = self._calcula_total(nivel, competencia)
+        if total <= TETO_INSS:
+            return 0
+        return round((total - TETO_INSS) * FATOR_PATRONAL_COMPLEMENTAR, 2)
+
+    def calcula(self, nivel: Nivel, competencia: date) -> Folha:
+        return Folha(
+            salario=self._calcula_salario(nivel),
+            anuenio=self._calcula_anuenio(competencia),
+            ats=self._calcula_ats(nivel),
+            total_antes_limite_prefeito=self._calcula_total_antes_limite_prefeito(
+                nivel, competencia
+            ),
+            total=self._calcula_total(nivel, competencia),
+            fufin_patronal=self._calcula_fufin_patronal(nivel, competencia),
+            bhprev_patronal=self._calcula_bhprev_patronal(nivel, competencia),
+            bhprev_complementar_patronal=self._calcula_bhprev_complementar_patronal(
+                nivel, competencia
+            ),
+        )
---
+++ tests/test_folha.py
@@ -0,0 +1,271 @@
+from datetime import date
+from src.classe import Classe
+from src.funcionario import TipoPrevidencia
+from src.nivel import Nivel
+from src.folha import *
+from src.tabela_salario import Tabela
+
+
+class MockTabela(Tabela):
+    def valor_do_nivel_para_classe(self, nivel: Nivel, classe: Classe) -> float:
+        if nivel == Nivel(1, "0") and classe == Classe.E2:
+            return 1000
+        if nivel == Nivel(25, "B") and classe == Classe.E2:
+            return 2000
+        if nivel == Nivel(30, "C") and classe == Classe.E2:
+            return LIMITE_PREFEITO
+        if nivel == Nivel(36, "D") and classe == Classe.E3:
+            return LIMITE_PROCURADORES
+        return 0.0
+
+
+class TestCalculoFolha:
+    def test_salario(self):
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=None,
+                num_ats=0,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.BHPrev,
+            ),
+        )
+        nivel = Nivel(25, "B")  # Valor: 2.000
+        assert calculadora.calcula(nivel, date(2023, 10, 1)).salario == 2000
+
+    def test_calcula_anuenio(self):
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=date(2000, 1, 1),
+                num_ats=0,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.BHPrev,
+            ),
+        )
+        competencia = date(2008, 2, 1)  # 8 anos. Nível(1, "0") é 1000
+        assert calculadora.calcula(Nivel(1, "0"), competencia).anuenio == 80
+
+    def test_calcula_ats(self):
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=None,
+                num_ats=3,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.BHPrev,
+            ),
+        )
+        nivel = Nivel(25, "B")  # Valor: 2.000
+        assert calculadora.calcula(nivel, date(2023, 10, 1)).ats == 60
+
+    def test_calcula_total_antes_limite_prefeito(self):
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=date(2000, 1, 1),
+                num_ats=3,  # R$ 60 de ats
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.BHPrev,
+            ),
+        )
+        nivel = Nivel(25, "B")  # Valor: 2.000
+        competencia = date(2008, 2, 1)  # 8 anos = 80 reais de anuênio
+        assert (
+            calculadora.calcula(nivel, competencia).total_antes_limite_prefeito == 2140
+        )
+
+    def test_calcula_total_quando_nao_atinge_teto(self):
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=date(2000, 1, 1),
+                num_ats=0,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.BHPrev,
+            ),
+        )
+        nivel = Nivel(25, "B")  # Valor: 2.000
+        competencia = date(2008, 2, 1)  # 8 anos = 80 reais de anuênio
+        assert calculadora.calcula(nivel, competencia).total == 2080
+
+    def test_total_respeita_limite_do_prefeito(self):
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=date(2000, 1, 1),
+                num_ats=0,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.BHPrev,
+            ),
+        )
+        nivel = Nivel(30, "C")  # Valor: LIMITE_PREFEITO
+        competencia = date(
+            2008, 2, 1
+        )  # 8 anos = 80 reais de anuênio -> passa do limite
+        assert calculadora.calcula(nivel, competencia).total == LIMITE_PREFEITO
+
+    def test_total_respeita_limite_do_procurador(self):
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E3,
+                data_anuenio=date(2000, 1, 1),
+                num_ats=0,
+                procurador=True,
+                tipo_previdencia=TipoPrevidencia.BHPrev,
+            ),
+        )
+        nivel = Nivel(36, "D")  # Valor: LIMITE_PROCURADORES
+        competencia = date(
+            2008, 2, 1
+        )  # 8 anos = 80 reais de anuênio -> passa do limite
+        assert calculadora.calcula(nivel, competencia).total == LIMITE_PROCURADORES
+
+    def test_fufin_eh_zero_se_bh_prev(self):
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=date(2000, 1, 1),
+                num_ats=0,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.BHPrev,
+            ),
+        )
+        assert calculadora.calcula(Nivel(1, "0"), date(2023, 10, 1)).fufin_patronal == 0
+
+    def test_fufin_eh_zero_se_bh_prev_complementar(self):
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=date(2000, 1, 1),
+                num_ats=0,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.BHPrevComplementar,
+            ),
+        )
+        assert calculadora.calcula(Nivel(1, "0"), date(2023, 10, 1)).fufin_patronal == 0
+
+    def test_fufin(self):
+        competencia = date(2023, 10, 1)
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=competencia,
+                num_ats=0,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.Fufin,
+            ),
+        )
+        nivel = Nivel(1, "0")  # Valor: 1.000
+        assert (
+            calculadora.calcula(nivel, competencia).fufin_patronal
+            == 1000 * FATOR_PATRONAL
+        )
+
+    def test_bhprev_patronal_passa_teto_inss_se_bhprev(self):
+        competencia = date(2023, 10, 1)
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=competencia,
+                num_ats=0,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.BHPrev,
+            ),
+        )
+        nivel = Nivel(30, "C")  # Valor: LIMITE_PREFEITO
+        assert calculadora.calcula(nivel, competencia).bhprev_patronal == round(
+            LIMITE_PREFEITO * FATOR_PATRONAL, 2
+        )
+
+    def test_bhprev_patronal_limitado_ao_inss_se_bhprev_complementar(self):
+        competencia = date(2023, 10, 1)
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=competencia,
+                num_ats=0,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.BHPrevComplementar,
+            ),
+        )
+        nivel = Nivel(30, "C")  # Valor: LIMITE_PREFEITO
+        assert calculadora.calcula(nivel, competencia).bhprev_patronal == round(
+            TETO_INSS * FATOR_PATRONAL, 2
+        )
+
+    def test_bhprev_complementar_zerada_se_fufin(self):
+        competencia = date(2023, 10, 1)
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=competencia,
+                num_ats=0,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.Fufin,
+            ),
+        )
+        nivel = Nivel(1, "0")  # Valor: 1.000
+        assert calculadora.calcula(nivel, competencia).bhprev_patronal == 0
+
+    def test_bhprev_complementar_zerada_se_bhprev(self):
+        competencia = date(2023, 10, 1)
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=competencia,
+                num_ats=0,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.BHPrev,
+            ),
+        )
+        nivel = Nivel(1, "0")  # Valor: 1.000
+        assert calculadora.calcula(nivel, competencia).bhprev_complementar_patronal == 0
+
+    def test_bhprev_complementar_zerada_se_nao_atinge_teto_inss(self):
+        competencia = date(2023, 10, 1)
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=competencia,
+                num_ats=0,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.BHPrevComplementar,
+            ),
+        )
+        nivel = Nivel(1, "0")  # Valor: 1.000
+        assert calculadora.calcula(nivel, competencia).bhprev_complementar_patronal == 0
+
+    def test_bhprev_complementar_somente_acima_teto_inss(self):
+        competencia = date(2023, 10, 1)
+        calculadora = CalculaFolha(
+            tabela=MockTabela(),
+            funcionario=DadosFolha(
+                classe=Classe.E2,
+                data_anuenio=competencia,
+                num_ats=0,
+                procurador=False,
+                tipo_previdencia=TipoPrevidencia.BHPrevComplementar,
+            ),
+        )
+        nivel = Nivel(30, "C")  # Valor: LIMITE_PREFEITO
+        assert calculadora.calcula(
+            nivel, competencia
+        ).bhprev_complementar_patronal == round(
+            (LIMITE_PREFEITO - TETO_INSS) * FATOR_PATRONAL_COMPLEMENTAR, 2
+        )
